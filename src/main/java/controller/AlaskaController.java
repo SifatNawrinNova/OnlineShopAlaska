bfb23ddf6bc44b7c242db7fcf049f501b9893c64
FaizaHuma <fhuma46@gmail.com>
2018-05-04 02:12:26 +0600

    commit5

--- src/main/java/controller/AlaskaController.java
+++ src/main/java/controller/AlaskaController.java
@@ -29,31 +29,32 @@ public class AlaskaController {
                 ResultSet rs=smt.executeQuery("Select email,password from signUp");
                 while(rs.next())
                 {
-                    if(lg.getUsername().equals(rs.getString("email"))) {
-                        if (lg.getPassword().equals(rs.getString("password"))) {
-                            String str1 = "insert into login(sign_up_id) select SIGN_UP_ID from SIGNUP where email='" + lg.getUsername() + "'";
-                            String str2 = "Update login set login_DateTime=systimestamp where SIGN_UP_ID=(select SIGN_UP_ID from SIGNUP where email='" + lg.getUsername() + "')";
+                    if(lg.getUsername().equals(rs.getString("email"))&&lg.getPassword().equals(rs.getString("password"))) {
+
+                        smt.executeUpdate("ALTER SESSION SET NLS_TIMESTAMP_FORMAT='YY-MON-DD HH24:MI:SS'");
+                        smt.executeUpdate("ALTER SESSION SET TIME_ZONE = '+6:00'");
+
+                        String str1 = "insert into login(sign_up_id) select SIGN_UP_ID from SIGNUP where email='" + lg.getUsername() + "'";
+                        String str2 = "Update login set login_DateTime=systimestamp where SIGN_UP_ID=(select SIGN_UP_ID from SIGNUP where email='" + lg.getUsername() + "')";
+                        String str3="delete from LOGIN where sign_up_id=(select sign_up_id from SIGNUP where email='"+lg.getUsername()+"')";
+                            smt.executeUpdate(str3);
                             smt.executeUpdate(str1);
                             smt.executeUpdate(str2);
                             return "view";
-                        }
-                        else
-                        {
-                            return "fail";
-                        }
                     }
                     else
                     {
 
-                        return "fail";
+                       continue;
                     }
 
 
                 }
                 rs.close();
                 con.close();
+                return "fail";
 
-            }
+        }
             catch (Exception e)
             {
                 String exp=String.valueOf(e);
